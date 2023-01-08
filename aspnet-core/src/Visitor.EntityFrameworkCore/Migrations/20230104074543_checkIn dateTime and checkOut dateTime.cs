9fe0755f02c8341f5bac41f81cdc3eca764e97cf
Hannan Fikri <hannanfikri510@gmail.com>
2023-01-08 18:21:47 +0800

    Merge branch 'master' of https://dev.azure.com/DigitalIntern/VisitorManagement/_git/VisitorManagement
    # Conflicts:
    #	angular/src/app/main/appointment/appointments/create-or-edit-appointment-modal.component.html
    #	angular/src/app/main/appointment/appointments/create-or-edit-appointment-modal.component.ts
    #	angular/src/app/main/appointment_Today/create-or-edit-appointment_Today-modal.component.ts
    #	angular/src/app/main/appointment_Tomorrow/create-or-edit-appointment_Tomorrow-modal.component.ts
    #	angular/src/app/main/appointment_Yesterday/create-or-edit-appointment_Yesterday-modal.component.ts
    #	angular/src/public/Pages/appointment/form-wizard/form-wizard.component.html
    #	angular/src/public/Pages/appointment/form-wizard/form-wizard.component.ts
    #	angular/src/shared/service-proxies/service-proxies.ts
    #	aspnet-core/src/Visitor.Application.Shared/Appointment/CreateOrEditAppointmentDto.cs

---
+++ aspnet-core/src/Visitor.EntityFrameworkCore/Migrations/20230104074543_checkIn dateTime and checkOut dateTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Visitor.Migrations
+{
+    public partial class checkIndateTimeandcheckOutdateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckInDateTime",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckOutDateTime",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutDateTime",
+                table: "Appointments");
+
+            migrationBuilder.RenameColumn(
+                name: "passNumber",
+                table: "Appointments",
+                newName: "PassNumber");
+        }
+    }
+}

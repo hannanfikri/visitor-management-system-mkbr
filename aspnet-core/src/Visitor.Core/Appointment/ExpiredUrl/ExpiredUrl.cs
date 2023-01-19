5f5a307cb95803c76c4f1ef0eeb026b729707125
Hafiy Kharu <muhamadhafiykharu@gmail.com>
2023-01-19 10:02:13 +0800

    expired url

---
+++ aspnet-core/src/Visitor.Core/Appointment/ExpiredUrl/ExpiredUrl.cs
@@ -0,0 +1,32 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor.Appointment.ExpiredUrl
+{
+    [Table("ExpiredUrls")]
+    public class ExpiredUrl:FullAuditedEntity<Guid>
+    {
+        [Required]
+        public virtual DateTime UrlCreateDate { get; set; }
+
+        [Required]
+        public virtual DateTime UrlExpiredDate { get; set; }
+
+        [Required]
+        public virtual string Item { get; set; }
+
+        [Required]
+        public virtual string Status { get; set; }
+
+        public virtual Guid? AppointmentId { get; set; }
+
+        [ForeignKey("AppointmentId")]
+        public AppointmentEnt AppointmentFk { get; set; }
+    }
+}

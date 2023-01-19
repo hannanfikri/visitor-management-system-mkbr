4c0c54290a04f1408f0293566eaa76c4f547f7e1
Hafiy Kharu <muhamadhafiykharu@gmail.com>
2023-01-19 10:27:35 +0800

    expired url

---
+++ aspnet-core/src/Visitor.EntityFrameworkCore/Migrations/20230119022613_expireUrl table3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Visitor.Migrations
+{
+    public partial class expireUrltable3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
